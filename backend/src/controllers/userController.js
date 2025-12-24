861ee07a7979a4d6f6df5c6c77663c835ce5dfce
Sahil Chouhan <chouhangolu604@gmail.com>
2025-12-24 15:53:03 +0530

    update

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -24,14 +24,6 @@ export const registerUser = async (req, res) => {
             role
         })
 
-        res.cookie("token", token, {
-            httpOnly: true,
-            secure: true,      // Render (HTTPS)
-            sameSite: "none",  // cross-origin
-            maxAge: 7 * 24 * 60 * 60 * 1000,
-        });
-
-
         res.status(201).json({ message: "User registered successfully", user: user })
     } catch (error) {
         console.log(`Error in regiter handle-------> ${error}`)
